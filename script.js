a9508917b358cef854d66f313d843fbb6e29706e
Pratik Patra <pratikpatra09@gmail.com>
2024-02-06 11:43:32 +0530

    Added lock & unlock functionality to text area

--- script.js
+++ script.js
@@ -59,11 +59,10 @@ removeBtn.addEventListener("click",function(){
 function createTicket(task,taskId,modalPriorityColor) {
     let ticketCont = document.createElement('div');
     ticketCont.setAttribute("class","ticket-cont");
-    ticketCont.innerHTML = `<div class="ticket-cont">
-                                <div class="ticket-color ${modalPriorityColor}"></div>
-                                <div class="ticket-id">#${taskId}</div>
-                                <div class="ticket-area">${task}</div>
-                            </div>`
+    ticketCont.innerHTML = `<div class="ticket-color ${modalPriorityColor}"></div>
+                            <div class="ticket-id">#${taskId}</div>
+                            <div class="ticket-area">${task}</div>
+                            <div class="lock-unlock"><i class="fa-solid fa-lock"></i></div>`
     mainCont.appendChild(ticketCont);
 
     //handle delete of ticket
@@ -90,5 +89,23 @@ function createTicket(task,taskId,modalPriorityColor) {
         ticketColor.classList.add(nextColor)
 
     })
+
+    //handle lock unlock button
+    let lockUnlockBtn = ticketCont.querySelector(".lock-unlock i");
+    let taskArea = ticketCont.querySelector(".ticket-area");
+    lockUnlockBtn.addEventListener("click",function(){
+        console.log(lockUnlockBtn);
+        if(lockUnlockBtn.classList.contains("fa-lock")){
+            lockUnlockBtn.classList.remove("fa-lock");
+            lockUnlockBtn.classList.add("fa-lock-open");
+            taskArea.setAttribute("contenteditable","true");
+        }
+        else if(lockUnlockBtn.classList.contains("fa-lock-open")){
+            lockUnlockBtn.classList.remove("fa-lock-open");
+            lockUnlockBtn.classList.add("fa-lock");
+            taskArea.setAttribute("contenteditable","false");
+        }
+    })
+
 }
 
